ed7c9e79ec93135a212ae67bdb77db72ed9c93a0
suisdavid <18217722446@163.com>
2025-11-20 15:10:18 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ using namespace antlr4;
 //       if you really need to regenerate,please ask TA for help.
 int main(int argc, const char *argv[]) {
 	// TODO: please don't modify the code below the construction of ifs if you want to use visitor mode
-	freopen("ans.out","w",stdout);
 	ANTLRInputStream input(std::cin);
 	Python3Lexer lexer(&input);
 	CommonTokenStream tokens(&lexer);
